d4392366bdd9a83053df7a1182066c36477f586d
paingsoeko <social.kopaing@gmail.com>
2024-08-21 01:47:47 +0700

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Paing Soe Ko on 21/8/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
